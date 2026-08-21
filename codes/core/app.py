7ab7589af363257e62cf685524b72e43a7f22226
finorako <finorako@student.42antananarivo.mg>
2026-08-21 22:50:38 +0300

    updated all core structure

--- codes/core/app.py
+++ codes/core/app.py
@@ -1,6 +1,25 @@
+import pygame
+
+
 class App:
     def __init__(self) -> None:
-        pass
+        self.screen = pygame.display.set_mode((1280, 950))
+        pygame.display.set_caption("Pacman")
 
     def run(self) -> None:
-        print("runing")
+        running: bool = True
+        clock = pygame.time.Clock()
+        while running:
+            dt = clock.tick() / 1000
+            self.draw(dt)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    break
+            self.update(dt)
+
+    def draw(self, dt: float) -> None:
+        self.screen.fill("white")
+
+    def update(self, dt: float) -> None:
+        pygame.display.update()
