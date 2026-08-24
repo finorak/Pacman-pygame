ae7068c41b3dadbff6899b17f6400bdbe2b433df
ScottyXBlaze <ramanantsoake@gmail.com>
2026-08-24 23:11:20 +0300

    refactor: Making the rendering file more obvious

---
+++ codes/rendering/screen/__init__.py
@@ -0,0 +1,13 @@
+from .base_screen import Screen
+from .game_screen import GameScreen
+from .highscore_screen import HighScoreScreen
+from .instructions_screen import InstructionsScreen
+from .home_screen import HomeScreen
+
+__all__ = [
+    "Screen",
+    "GameScreen",
+    "HighScoreScreen",
+    "InstructionsScreen",
+    "HomeScreen",
+]
--- codes/rendering/screen/game_screen.py
+++ codes/rendering/screen/game_screen.py
@@ -1,14 +1,12 @@
-from .base import Screen
+from .base_screen import Screen
+
 
 class GameScreen(Screen):
     def __init__(self) -> None:
         super().__init__()
 
-    def get_input(self) -> None:
-        ...
+    def get_input(self) -> None: ...
 
-    def update(self, dt: float) -> None:
-        ...
+    def update(self, dt: float) -> None: ...
 
-    def render(self) -> None:
-        ...
+    def render(self) -> None: ...
