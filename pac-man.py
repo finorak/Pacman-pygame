7642dd1247769a916890b07ed828925d693be046
ScottyXBlaze <ramanantsoake@gmail.com>
2026-08-24 22:53:42 +0300

    feat: Starting the base of the rendering

--- pac-man.py
+++ pac-man.py
@@ -4,8 +4,9 @@ from codes.core.app import App
 
 
 def main() -> None:
-    if len(sys.argv) < 1:
-        print("Pleas provde argument", file=sys.stderr)
+    if len(sys.argv) != 2:
+        print("[ERROR] Invalid arguments.", file=sys.stderr)
+        print(f"[USAGE] python3 {sys.argv[0]} <config>", file=sys.stderr)
         sys.exit(1)
     app = App(sys.argv[1], (950, 950))
     app.run()
