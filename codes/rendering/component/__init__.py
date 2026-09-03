07712ec4965596ffcb0a2fc42f38eb6a63f4d0c1
nyramana <nyramana@student.42antananarivo.mg>
2026-09-03 14:51:00 +0300

    feat: Starting the button class

--- codes/rendering/component/__init__.py
+++ codes/rendering/component/__init__.py
@@ -3,4 +3,5 @@ from .maze import Maze
 from .player import Player
 from .sprite import AnimatedSprite, Sprite
 
-__all__ = ["AnimatedSprite", "Maze", "Player", "Sprite", "Button"]
+__all__ = ["AnimatedSprite", "Button", "Maze", "Player", "Sprite"]
+
--- codes/rendering/screen/home_screen.py
+++ codes/rendering/screen/home_screen.py
@@ -2,7 +2,7 @@ import pygame
 
 from codes.rendering.component.sprite import Sprite
 
-from ..component import AnimatedSprite
+from ..component import AnimatedSprite, Button
 from .base_screen import Screen
 
 
@@ -10,19 +10,26 @@ class HomeScreen(Screen):
     def __init__(self) -> None:
         super().__init__()
         self.assets: dict[str, AnimatedSprite] = {}
+        self.buttons: dict[str, Button] = {}
         self.load_background()
+        self.load_buttons()
         self.load_others()
 
     def get_input(self) -> None: ...
 
     def update(self, dt: float) -> None:
         self.background.rect.x -= 20 * dt
+        for button in self.buttons.values():
+            button.update(dt)
+
 
     def render(self, screen: pygame.Surface) -> None:
         self._render_background(screen)
         screen.blit(self.logo.image, self.logo.rect)
         for a in self.assets.values():
             screen.blit(a.image, a.rect)
+        for a in self.buttons.values():
+            a.draw(screen)
 
     def load_assets(self) -> dict[str, AnimatedSprite]:
         image_path = {"logo": ("assets", "Logo")}
@@ -32,16 +39,31 @@ class HomeScreen(Screen):
             )
         return self.assets
 
-    def load_buttons(self) -> None: ...
+    def load_buttons(self) -> None:
+        buttons = {"start": (220, 250), "exit": (220, 310)}
+        for button, pos in buttons.items():
+            tmp = {}
+            tmp["normal"] = AnimatedSprite(
+                pos,
+                [self.loader.import_image("assets", "button", button, "1")],
+            )
+            tmp["hover"] = AnimatedSprite(
+                pos,
+                [self.loader.import_image("assets", "button", button, "2")],
+            )
+            tmp["pressed"] = AnimatedSprite(
+                pos,
+                self.loader.import_folder("assets", "button", button),
+            )
+            a = Button(pos, tmp, button)
+            self.buttons[button] = a
 
     def load_background(self) -> None:
         self.background = Sprite(
             (0, 0), self.loader.import_image("assets", "background")
         )
         print(self.background.rect.height)
-        self.background.image = pygame.transform.scale2x(
-            self.background.image
-        )
+        self.background.image = pygame.transform.scale2x(self.background.image)
         self.background.rect = self.background.image.get_frect()
 
     def load_others(self) -> None:
