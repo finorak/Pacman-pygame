05f5b98eb58c660b4af3bf3aaaf3cc35e37e1505
nyramana <nyramana@student.42antananarivo.mg>
2026-09-08 12:56:41 +0300

    feat: Adding the basic loader for the highscore system

--- codes/highscore/loader.py
+++ codes/highscore/loader.py
@@ -7,12 +7,31 @@ from .model import HighScoreModel
 class HighScoreLoader:
     def __init__(self, path: Path) -> None:
         self.path = path
+        self.highscore = self.load()
 
     def load(self) -> list[HighScoreModel]:
         with open(self.path) as file:
             highscore = json.load(file)
-        return [HighScoreModel.model_validate(value) for value in highscore]
+        highscores = [
+            HighScoreModel.model_validate(value) for value in highscore
+        ]
+        return sorted(highscores, key=lambda x: x.player_score, reverse=True)
 
     def save(self, models: list[HighScoreModel]) -> None:
         with open(self.path, "w") as file:
-            file.write(json.dumps([model.model_dump_json() for model in models]))
+            file.write(
+                json.dumps([model.model_dump_json() for model in models])
+            )
+
+    def update(self) -> None:
+        self.highscore = sorted(
+            self.highscore, key=lambda x: x.player_score, reverse=True
+        )
+
+    def add_score(self, name: str, score: int) -> None:
+        self.highscore = sorted(
+            self.highscore
+            + [HighScoreModel(player_name=name, player_score=score)],
+            key=lambda x: x.player_score,
+            reverse=True,
+        )[:10]
