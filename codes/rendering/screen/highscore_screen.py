98bbb9aa049d9c36b68f1c45d079a263d60df10b
nyramana <nyramana@student.42antananarivo.mg>
2026-09-16 14:15:41 +0300

    feat: Adding the highscore color and handling the file error

--- codes/rendering/screen/highscore_screen.py
+++ codes/rendering/screen/highscore_screen.py
@@ -44,11 +44,11 @@ class HighScoreScreen(Screen):
         screen.blit(self.logo.image, self.logo.rect)
         for a in self.buttons.values():
             a.draw(screen)
-        screen.blit(self.leaderboard, (100, 100))
+        screen.blit(self.leaderboard)
 
     def load_logo(self) -> AnimatedSprite:
         path = ("assets", "highscore", "Logo")
-        return AnimatedSprite((100, 40), [self.loader.import_image(*path)])
+        return AnimatedSprite((470, 50), [self.loader.import_image(*path)])
 
     def draw_leaderboard(self) -> pygame.Surface:
         surface = pygame.Surface(
@@ -56,12 +56,34 @@ class HighScoreScreen(Screen):
         ).convert_alpha()
         surface.fill((0, 0, 0, 0))
         for i, score in enumerate(self.highscore_loader.highscore, 1):
-            text = self.fonts.render(
-                f"{i:02} - {score.player_name}: {score.player_score}",
-                True,
-                "yellow",
+            if i == 1:
+                text = self.fonts.render(
+                    f"{i:02} - {score.player_name}: {score.player_score}",
+                    True,
+                    "red",
+                )
+            elif i == 2:
+                text = self.fonts.render(
+                    f"{i:02} - {score.player_name}: {score.player_score}",
+                    True,
+                    "gold",
+                )
+            elif i == 3:
+                text = self.fonts.render(
+                    f"{i:02} - {score.player_name}: {score.player_score}",
+                    True,
+                    "pink",
+                )
+            else:
+                text = self.fonts.render(
+                    f"{i:02} - {score.player_name}: {score.player_score}",
+                    True,
+                    (230, 230, 230),
+                )
+            surface.blit(
+                text,
+                (self.get_center(text.width), i * 50 + 100),
             )
-            surface.blit(text, (0, i * 50))
         return surface
 
     def load_buttons(self) -> None:
