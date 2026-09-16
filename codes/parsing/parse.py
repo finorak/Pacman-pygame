82535ff1f121d71cf6e921fa1530dd4616321c73
nyramana <nyramana@student.42antananarivo.mg>
2026-09-16 14:28:59 +0300

    feat: Making the parsing clearer

--- codes/parsing/parse.py
+++ codes/parsing/parse.py
@@ -1,6 +1,9 @@
+from typing import Self
+
 from pydantic import (
     BaseModel,
     Field,
+    model_validator,
 )
 
 
@@ -10,11 +13,28 @@ class ConfigError(Exception):
 
 
 class GameModel(BaseModel):
-    pacgum_number: int = Field(ge=1, lt=50)
-    points_per_pacgum: int = Field(ge=1)
-    points_per_super_pacgum: int = Field(ge=1)
-    points_per_ghost: int = Field(ge=1)
-    level_max_time: int = Field(ge=5, le=90)
-    life: int = Field(ge=3, le=10)
-    seed: int = Field(default=42, ge=1)
-    levels: list[list[int]] = Field(min_length=1)
+    pacgum_number: int = Field()
+    points_per_pacgum: int = Field()
+    points_per_super_pacgum: int = Field()
+    points_per_ghost: int = Field()
+    level_max_time: int = Field()
+    life: int = Field()
+    seed: int = Field()
+    levels: list[list[int]] = Field()
+
+    @model_validator(mode="after")
+    def check_numbers(self) -> Self:
+        if self.pacgum_number > 343 or self.pacgum_number <= 0:
+            raise ValueError("Pacgum number must be between 343 and 1")
+        if self.points_per_pacgum > 1000 or self.points_per_pacgum <= 0:
+            raise ValueError("Pacgum point should be between 1000 and 1")
+        if (
+            self.points_per_super_pacgum > 1000
+            or self.points_per_super_pacgum <= 0
+        ):
+            raise ValueError("Super pacgum point should be between 1000 and 1")
+        if self.level_max_time > 1000 or self.level_max_time <= 60:
+            raise ValueError("Max time should be between 1000 and 60")
+        if self.life > 10 or self.life <= 0:
+            raise ValueError("Life count should be between 10 and 1")
+        return self
