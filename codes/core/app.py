9401c90e33386d31b3d721ac85a2a431c7d8b889
nyramana <nyramana@student.42antananarivo.mg>
2026-09-22 15:53:37 +0300

    feat: Solving linting

--- codes/core/app.py
+++ codes/core/app.py
@@ -1,4 +1,5 @@
 """Module used to combine our implementatoin."""
+
 import pygame
 
 from codes.data.data import Data
@@ -23,9 +24,8 @@ class Rendering:
 project."""
 
     def __init__(
-            self,
-            screen_size: tuple[int, int],
-            config_file: str | None) -> None:
+        self, screen_size: tuple[int, int], config_file: str | None
+    ) -> None:
         """Initialize a `Rendering` class instance.
 
         Args:
@@ -113,7 +113,9 @@ in this case, we do them in each screen fo better mantainability.
             (0, 0), SpriteLoader.import_image("assets", "background")
         )
         self.background.image = pygame.transform.scale2x(self.background.image)
-        self.background.rect = Frect(0, 0, self.background.image.width, self.background.image.height)
+        self.background.rect = Frect(
+            0, 0, self.background.image.width, self.background.image.height
+        )
 
     def _render_background(self, screen: pygame.Surface) -> None:
         image_width = self.background.rect.width
